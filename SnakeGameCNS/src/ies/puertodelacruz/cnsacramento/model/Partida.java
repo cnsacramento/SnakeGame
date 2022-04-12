7b2e7dbf079400a7ee81dfd566a6a79166f09baf
Christian Novo Sacramento <christianivh366@gmail.com>
2022-04-12 13:38:35 +0100

    Se comienza la estructura de la clase Partida

---
+++ SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/model/Partida.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.puertodelacruz.cnsacramento.model;
+
+/**
+ * Clase encargada de gestionar la partida que se juega
+ * @author christian
+ */
+public class Partida {
+    
+    private String estado;
+    private Escenario escenario;
+    
+    /* METODOS */
+    
+    /**
+     * Metodo que empieza la partida
+     */
+    public void empezarPartida() {
+        
+    }
+    
+    /**
+     * Metodo que finaliza la partida
+     */
+    public void finalizarPartida() {
+        
+    }
+    
+    /***
+     * Metodo que indica que la partida se gano
+     */
+    public void ganarPartida() {
+        
+    }
+    
+    /**
+     * Metodo que indica que la partida se perdio
+     */
+    public void perderPartida() {
+        
+    }
+}
