af5a7f4d533d90ff2ec2da8812545561a38a3743
Christian Novo Sacramento <christianivh366@gmail.com>
2022-04-12 13:31:57 +0100

    Se añade la estructura de la clase Escenario

---
+++ SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/model/Escenario.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.puertodelacruz.cnsacramento.model;
+
+/**
+ * Clase encargada del escenario
+ * @author christian
+ */
+public class Escenario {
+    
+    private Serpiente serpiente;
+    private int tamanioX;
+    private int tamanioY;
+    
+    
+    /* METODOS */
+    
+    /*
+    * Metodo encargado de detectar colisiones 
+    */
+    public void detectarColision() {
+        
+    }
+    
+    /**
+     * Metodo encargado de detectar cuando la serpiente come
+     */
+    public void detectarDigestion() {
+        
+    }
+    
+    /* GETTERS Y SETTERS */
+    
+    public Serpiente getSerpiente() {
+        return this.serpiente;
+    }
+    
+    public void setSerpiente(Serpiente serpiente) {
+        this.serpiente = serpiente;
+    }
+    
+    public int getTamanioX() {
+        return this.tamanioX;
+    } 
+    
+    public void setTamanioX(int tamanioX) {
+        this.tamanioX = tamanioX;
+    }
+    
+    public int getTamanioY() {
+        return this.tamanioY;
+    }
+    
+    public void setTamanioY(int tamanioY) {
+        this.tamanioY = tamanioY;
+    }
+}
