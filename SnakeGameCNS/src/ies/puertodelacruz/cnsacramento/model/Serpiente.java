01fe839062a049f043df0580f80d54731239c6bb
Christian Novo Sacramento <christianivh366@gmail.com>
2022-04-12 13:23:20 +0100

    Se crea la estructura de la clase serpiente

--- SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/model/Serpiente.java
+++ SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/model/Serpiente.java
@@ -4,10 +4,59 @@
  */
 package ies.puertodelacruz.cnsacramento.model;
 
+import java.util.LinkedList;
+
 /**
- *
+ * 
  * @author christian
  */
 public class Serpiente {
     
+    private int posicionX;
+    private int posicionY;
+    private LinkedList<Integer> tamanio;
+    
+    
+    /* METODOS */
+    
+    /**
+     * Metodo encargado del movimiento de la serpiente
+     */
+    public void mover() {
+        
+    }
+    
+    /**
+     * Metodo encargado de aumentar el tamanio de la serpiente 
+     */
+    public void aumentarTamnio() {
+        
+    }
+    
+    
+    /* GETTERS Y SETTERS */
+    
+    public int getPosicionX() {
+        return this.posicionX;
+    }
+    
+    public void setPosicionX(int posicionX) {
+        this.posicionX = posicionX;
+    }
+    
+    public int getPosicionY() {
+        return this.posicionY;
+    }
+    
+    public void setPosicionY(int posicionY) {
+        this.posicionY = posicionY;
+    }
+    
+    public LinkedList<Integer> getTamanio() {
+        return this.tamanio;
+    }
+    
+    public void setTamanio(LinkedList<Integer> tamanio) {
+        this.tamanio = tamanio;
+    }
 }
