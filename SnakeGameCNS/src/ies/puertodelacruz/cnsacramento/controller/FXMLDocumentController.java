a9c1cab0c04d6fb74df87d9d30dcb98075ab97ff
Christian Novo Sacramento <christianivh366@gmail.com>
2022-04-13 14:26:33 +0100

    Colision con todas los bordes conseguido, solucionando primer movimiento

--- SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
+++ SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
@@ -50,12 +50,10 @@ public class FXMLDocumentController implements Initializable {
 
     
     public void mostrarGraficos() {
-        
         final double VELOCIDAD_JUEGO = 100;
         Timeline fps = new Timeline();
         fps.setCycleCount(Animation.INDEFINITE);
         fps.getKeyFrames().add(new KeyFrame(Duration.millis(VELOCIDAD_JUEGO), (t) -> {
-            
             graficos.clearRect(0, 0, escenarioCanvas.getWidth(), escenarioCanvas.getHeight());
             if(escenario.getColisionDetectada()) {
                 graficos.fillRect(serpiente.getPosicionAnteriorX(), serpiente.getPosicionAnteriorY(), dimensionSerpiente, dimensionSerpiente);
@@ -64,6 +62,7 @@ public class FXMLDocumentController implements Initializable {
                 
             }
             System.out.println(escenario.getColisionDetectada());
+            
             serpiente.continuarMoviendo(dimensionSerpiente);
             escenario.detectarColision();
             graficos.fillRect(serpiente.getPosicionX(), serpiente.getPosicionY(), dimensionSerpiente, dimensionSerpiente);
@@ -73,12 +72,11 @@ public class FXMLDocumentController implements Initializable {
         fps.play();
         
     }
-
+    
     @FXML
     private void moverSerpiente(KeyEvent event) {
         
         KeyCode tecla = event.getCode();
-        
         switch(tecla) {
             case UP:
                 System.out.println("Subiendo...");
