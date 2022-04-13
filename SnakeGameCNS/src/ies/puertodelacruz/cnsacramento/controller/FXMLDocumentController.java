e5cce286b967d4899f929294e098e3b21b7f7dbe
Christian Novo Sacramento <christianivh366@gmail.com>
2022-04-13 11:59:43 +0100

    Se añade movimiento del canvas al pulsar las teclas

--- SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
+++ SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
@@ -8,12 +8,16 @@ import ies.puertodelacruz.cnsacramento.model.Partida;
 import ies.puertodelacruz.cnsacramento.model.Serpiente;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
 
 /**
  *
@@ -25,7 +29,7 @@ public class FXMLDocumentController implements Initializable {
     private Canvas escenarioCanvas;
     private GraphicsContext graficos;
     private Partida partida;
-    private double dimensionSerpiente = 10;
+    private final double dimensionSerpiente = 10;
     private Serpiente serpiente;
     
     @Override
@@ -38,15 +42,25 @@ public class FXMLDocumentController implements Initializable {
         System.out.println("Escenario tamaño X -> " + partida.getEscenario().getTamanioX());
         System.out.println("Escenario tamaño Y -> " + partida.getEscenario().getTamanioY());
         graficos = escenarioCanvas.getGraphicsContext2D();
+        double posicionXserpiente = partida.getEscenario().getSerpiente().getPosicionX();
+        double posicionYserpiente = partida.getEscenario().getSerpiente().getPosicionY();
+        graficos.fillRect(posicionXserpiente, posicionYserpiente, dimensionSerpiente, dimensionSerpiente);
         mostrarGraficos();
     }
 
     
     public void mostrarGraficos() {
         
-        double posicionXserpiente = partida.getEscenario().getSerpiente().getPosicionX();
-        double posicionYserpiente = partida.getEscenario().getSerpiente().getPosicionY();
-        graficos.fillRect(posicionXserpiente, posicionYserpiente, dimensionSerpiente, dimensionSerpiente);
+        final double VELOCIDAD_JUEGO = 100;
+        Timeline fps = new Timeline();
+        fps.setCycleCount(Animation.INDEFINITE);
+        fps.getKeyFrames().add(new KeyFrame(Duration.millis(VELOCIDAD_JUEGO), (t) -> {
+            
+            graficos.clearRect(0, 0, escenarioCanvas.getWidth(), escenarioCanvas.getHeight());
+            graficos.fillRect(serpiente.getPosicionX(), serpiente.getPosicionY(), dimensionSerpiente, dimensionSerpiente);
+        }));
+        
+        fps.play();
     }
 
     @FXML
