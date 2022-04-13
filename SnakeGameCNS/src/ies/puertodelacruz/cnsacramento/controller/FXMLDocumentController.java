85d528c465ad8d0d9bea4dc226938251b49f79ba
Christian Novo Sacramento <christianivh366@gmail.com>
2022-04-13 10:32:36 +0100

    Se añade el canvas, se incializa la partida y se elimina button y label

--- SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
+++ SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
@@ -4,20 +4,39 @@
  */
 package ies.puertodelacruz.cnsacramento.controller;
 
+import ies.puertodelacruz.cnsacramento.model.Partida;
+import java.awt.GraphicsConfiguration;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 /**
  *
  * @author christian
  */
 public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Canvas escenarioCanvas;
+    private GraphicsContext graficos;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        Partida partida = new Partida();
+        double ancho = escenarioCanvas.getWidth();
+        double alto = escenarioCanvas.getHeight();
+        partida.empezarPartida(ancho, alto);
+        graficos = escenarioCanvas.getGraphicsContext2D();
+        mostrarGraficos();
+    }
+
+    
+    public void mostrarGraficos() {
+        
+        graficos.fillRect(0, 0, 10, 10);
+    }
     
 }
