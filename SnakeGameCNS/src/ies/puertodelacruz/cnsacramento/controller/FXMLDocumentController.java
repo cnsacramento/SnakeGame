9914d2d4f4283af2edec6fd828043fc79b69af50
Christian Novo Sacramento <christianivh366@gmail.com>
2022-04-13 13:22:47 +0100

    Intentado detectar las colisiones

--- SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
+++ SnakeGameCNS/src/ies/puertodelacruz/cnsacramento/controller/FXMLDocumentController.java
@@ -57,11 +57,21 @@ public class FXMLDocumentController implements Initializable {
         fps.getKeyFrames().add(new KeyFrame(Duration.millis(VELOCIDAD_JUEGO), (t) -> {
             
             graficos.clearRect(0, 0, escenarioCanvas.getWidth(), escenarioCanvas.getHeight());
+            if(escenario.getColisionDetectada()) {
+                graficos.fillRect(serpiente.getPosicionAnteriorX(), serpiente.getPosicionAnteriorY(), dimensionSerpiente, dimensionSerpiente);
+                fps.stop();
+                System.out.println("GAME OVER");
+                
+            }
+            System.out.println(escenario.getColisionDetectada());
             serpiente.continuarMoviendo(dimensionSerpiente);
+            escenario.detectarColision();
             graficos.fillRect(serpiente.getPosicionX(), serpiente.getPosicionY(), dimensionSerpiente, dimensionSerpiente);
+            //System.out.println(escenario.detectarColision());
         }));
         
         fps.play();
+        
     }
 
     @FXML
